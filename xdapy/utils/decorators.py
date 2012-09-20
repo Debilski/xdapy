c14c0db972e8d979b9c20e81dc427ca167c4f276
Rike-Benjamin Schuppner <rikebs@debilski.de>
2012-09-20 14:48:45 +0200

    ENH: Add usage to autoappend decorator.

--- xdapy/utils/decorators.py
+++ xdapy/utils/decorators.py
@@ -6,7 +6,23 @@
 __authors__ = ['"Rike-Benjamin Schuppner" <rikebs@debilski.de>']
 
 def autoappend(a_list):
-    """Decorator which automatically appends the decorated class or method to a_list."""
+    """ Decorator which automatically appends the decorated class or method to a_list.
+
+    Usage::
+
+        classes = []
+
+        @autoappend(classes)
+        class A(object):
+            pass
+
+        @autoappend(classes)
+        class B(object):
+            pass
+
+        assert classes == [A, B]
+
+    """
     def wrapper(obj):
         a_list.append(obj)
         return obj
